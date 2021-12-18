0bde9ea2c5f1ffa923453c9ac34264c80a6b03a9
yim58 <yim58@DESKTOP-0UCDELL>
2021-12-18 12:56:04 +0900

    로그인 / 메뉴바 버튼 클릭시 색상변경(미완) 스크립트 추가 -im-

---
+++ GDJ41_JJajo_semi/src/main/webapp/js/script.js
@@ -0,0 +1,30 @@
+	/* 로그인 입력시, 아이디/ 비밀번호 공란 입력시 안내문구 출력 */
+      	const loginDo= ()=>{
+      		if(document.getElementsByName("memberId")[0].value==""
+      		||document.getElementsByName("memberPwd")[0].value==""){
+      			alert("내용을 입력해주세요!");
+      			return false;
+      		}else{
+      			document.loginSubmit.submit();
+      		}
+      		
+      	}
+  
+
+   		/* 메뉴바 선택 시, active값 자동변경 */
+      	$(function () { 
+	    	// actvie 활성화 
+		    $(".nav-item > .active").css("color", "white"); 
+		    
+		    $('.nav-link').click(function () { 
+		    	
+		    // .nav-link 클릭시 이전의 active 값 해제
+		    $(".nav-item > .active").css("color", "#007bff");  
+		    $('.nav-link').removeClass('active'); 
+		    
+		    // 클릭한 위치 active 적용 
+		    $(this).addClass('active'); 
+		    $(".nav-item > .active").css("color", "white"); 
+	    
+	    	}); 
+	  }); 
