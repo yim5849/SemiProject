e8369a269756ade121724f6aa42dfc6be93d7cdf
yim58 <yim58@DESKTOP-0UCDELL>
2021-12-21 16:27:07 +0900

    챌린져스 Dao클래스 작업 -im-

--- GDJ41_JJajo_semi/src/main/java/com/im/challengers/model/dao/CH_ChallengersDao.java
+++ GDJ41_JJajo_semi/src/main/java/com/im/challengers/model/dao/CH_ChallengersDao.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import com.im.challengers.model.vo.CH_Challengers;
@@ -69,6 +71,47 @@ public class CH_ChallengersDao {
 	
 /* ================================챌린져스 출력 (페이징 처리)============================= */	
 
+	public List<CH_Challengers> searchAllChallengers(Connection conn, int cPage, int numPerPage){
+		
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		CH_Challengers ch=null;
+		
+		List<CH_Challengers> list = new ArrayList();
+		
+		String sql=prop.getProperty("searchAllChallengers");
+		
+		try {
+			
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, (cPage-1)*numPerPage+1);
+			pstmt.setInt(2, cPage*numPerPage);
+			
+			rs=pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ch=CH_Challengers.builder()
+						.challengersNo(rs.getInt("CH_NO"))
+						.title(rs.getString("CH_TITLE"))
+						.content(rs.getString("CH_CONTENT"))
+						.filepath(rs.getString("CH_FILEPATH"))
+						.enrolldate(rs.getDate("CH_ENROLLDATE"))
+						.build();
+				list.add(ch);
+			}
+			
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(rs);
+			close(pstmt);
+		}
+		return list;
+		
+	}
 	
 /* ======================================챌린져스 등록================================= */	
 
@@ -82,10 +125,11 @@ public class CH_ChallengersDao {
 		try {
 			
 			pstmt=conn.prepareStatement(sql);
-			pstmt.setString(1, sql);
-			pstmt.setString(2, sql);
-			pstmt.setString(3, sql);
+			pstmt.setString(1, ch.getTitle());
+			pstmt.setString(2, ch.getContent());
+			pstmt.setString(3, ch.getFilepath());
 			
+			result=pstmt.executeUpdate();
 			
 		}catch(SQLException e) {
 			e.printStackTrace();
@@ -99,9 +143,52 @@ public class CH_ChallengersDao {
 	
 /* ======================================챌린져스 수정================================= */	
 
+	public int updateChallengers(Connection conn, CH_Challengers ch) {
+		
+		PreparedStatement pstmt=null;
+		int result=0;
+		
+		String sql=prop.getProperty("updateChallengers");
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, ch.getTitle());
+			pstmt.setString(2, ch.getContent());
+			pstmt.setString(3, ch.getFilepath());
+			pstmt.setInt(4, ch.getChallengersNo());
+			
+			result=pstmt.executeUpdate();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}return result;
+		
+	}
 	
 /* ======================================챌린져스 삭제================================= */	
 
+	public int deleteChallengers(Connection conn, int chNo) {
+		
+		PreparedStatement pstmt=null;
+		int result=0;
+		
+		String sql=prop.getProperty("deleteChallengers");
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, chNo);
+			
+			result=pstmt.executeUpdate();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}return result;
+		
+	}
 	
 /* ================================================================================= */	
 
