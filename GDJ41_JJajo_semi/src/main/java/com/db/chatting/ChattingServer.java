6cb5078efbcf99c0b09c6a41c828f039867497c7
DaBinSeo <87168961+DaBinSeo@users.noreply.github.com>
2021-12-27 12:13:40 +0900

    채팅... 전체만...

--- GDJ41_JJajo_semi/src/main/java/com/db/chatting/ChattingServer.java
+++ GDJ41_JJajo_semi/src/main/java/com/db/chatting/ChattingServer.java
@@ -1,6 +1,7 @@
 package com.db.chatting;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -15,6 +16,8 @@ import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 import com.db.chatting.model.vo.Message;
+
+import lombok.Synchronized;
 
 @ServerEndpoint(value="/jjajo/chatting",
 		encoders= {
@@ -38,34 +41,53 @@ public class ChattingServer {
 
 	public void message(Session session, Message msg) {   //msg 지금 온거 
 	
-		if(msg.getSender().equals(msg.getReceiver())) {
-			userList.put(session, msg.getMsg());
-			sendMsgAll(session, new Message("server","",msg.getMsg()+"님이 입장하셨습니다."));
-			return;
-		}
-		
+//		if(msg.getSender().equals(msg.getReceiver())) {
+//			
+//			userList.put(session, msg.getMsg());
+//			sendMsgAll(session, new Message("server","",msg.getMsg()+"님이 입장하셨습니다."));
+//			System.out.println("접속자 추가 "+msg.getReceiver());
+//			System.out.println("접속자 수 "+ userList.size());
+//			return;
+//		}
+//		
 
 		System.out.println(msg);
 		if(msg.getReceiver().equals("ALL")) {
-			sendMsgAll(session, msg);
-		}else {
-			Set<Session> keys = userList.keySet();
+			Set<Session> clients = session.getOpenSessions();
 			
-			Iterator<Session> it = keys.iterator();
-			
-			while(it.hasNext()) {
-				Session keySession = it.next();
-				String userId = userList.get(keySession);
-				if(msg.getReceiver().equals(userId)) {
+			for(Session client : clients) {
+				if(!client.equals(session)) {
 					try {
-						keySession.getBasicRemote().sendObject(msg);						
+						client.getBasicRemote().sendObject(msg);
 					}catch(IOException|EncodeException e) {
 						e.printStackTrace();
 					}
+					
 				}
+					
 			}
 			
 			
+			//sendMsgAll(session, msg);
+			
+		}else {
+//			Set<Session> keys = userList.keySet();
+//			
+//			Iterator<Session> it = keys.iterator();
+//			
+//			while(it.hasNext()) {
+//				Session keySession = it.next();
+//				String userId = userList.get(keySession);
+//				if(msg.getReceiver().equals(userId)) {
+//					try {
+//						keySession.getBasicRemote().sendObject(msg);						
+//					}catch(IOException|EncodeException e) {
+//						e.printStackTrace();
+//					}
+//				}
+//			}
+//			
+//			
 			
 		}
 		
@@ -82,6 +104,8 @@ public class ChattingServer {
 		Set<Session> keys = userList.keySet();
 		System.out.println("send");
 		for(Session s : keys) {
+			System.out.println("s   " + s);
+			System.out.println("s   " + session);
 			if(!s.equals(session)) {
 				try {
 					s.getBasicRemote().sendObject(msg);					
